ce0a8d14f26730f45712baffccc54fe3bf3354c9
Matthias Lee <matthiasclee09@gmail.com>
2021-12-21 12:19:47 -0800

    Update server.rb

--- server.rb
+++ server.rb
@@ -9,31 +9,31 @@ loop do
 	puts "Waiting for connection"
 	begin
 		client = server.accept
+		if client.gets.chomp == "RShell_Open"
+			currentDirCommand = client.gets
+			sock_domain, remote_port, remote_hostname, remote_ip = client.peeraddr
+			puts "Received connection from #{remote_ip}"
+			loop do
+				client.puts "whoami"
+				whoami = client.gets.gsub("\\NEWLINE", "\n").chomp.chomp
+				client.puts currentDirCommand
+				cd = client.gets.gsub("\\NEWLINE", "\n").chomp.chomp
+				print "#{whoami}@#{remote_ip}:#{cd}> "
+				cmd = gets.chomp
+				client.puts cmd
+				if cmd.downcase == "exit"
+					puts "Exiting..."
+					break
+				end
+				out = client.gets.chomp.gsub("\\NEWLINE", "\n")
+				print out
+			end
+		else
+			client.close
+		end
 	rescue Interrupt
 		client.puts "exit"
 		puts "\nExiting..."
 		exit
-	end
-	if client.gets.chomp == "RShell_Open"
-		currentDirCommand = client.gets
-		sock_domain, remote_port, remote_hostname, remote_ip = client.peeraddr
-		puts "Received connection from #{remote_ip}"
-		loop do
-			client.puts "whoami"
-			whoami = client.gets.gsub("\\NEWLINE", "\n").chomp.chomp
-			client.puts currentDirCommand
-			cd = client.gets.gsub("\\NEWLINE", "\n").chomp.chomp
-			print "#{whoami}@#{remote_ip}:#{cd}> "
-			cmd = gets.chomp
-			client.puts cmd
-			if cmd.downcase == "exit"
-				puts "Exiting..."
-				break
-			end
-			out = client.gets.chomp.gsub("\\NEWLINE", "\n")
-			print out
-		end
-	else
-		client.close
 	end
 end
